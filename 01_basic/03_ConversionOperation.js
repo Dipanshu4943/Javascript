b1a04318863857e6e14e99b7e5f602333639bb55
Dipanshu Chauhan <108367352+Dipanshu4943@users.noreply.github.com>
2024-01-08 14:29:40 +0000

    Conversion operation

---
+++ 01_basic/03_ConversionOperation.js
@@ -0,0 +1,62 @@
+/*let score = 33
+console.log(typeof score);
+console.log(typeof (score));*/
+
+/*let score = "33"
+console.log(typeof score);
+console.log(typeof (score));*/
+
+
+/*let score = "33"
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);*/
+
+/*let score = "33abc"
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)*/
+
+/*let score = null
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)*/
+
+/*let score = undefined
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)*/
+
+/*let score = true
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)*/
+
+
+/*let score = "Deep Rajput"
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber);*/
+
+let score = "Deep Rajput"
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber);
