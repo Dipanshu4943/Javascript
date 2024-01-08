6e1a8119d90f3503f44d6a0860253abd8e9858b9
Dipanshu Chauhan <108367352+Dipanshu4943@users.noreply.github.com>
2024-01-08 13:15:44 +0000

    Container config done

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,21 @@
+"Use strict"; // treat all js code as newer version.
+//alert (3+3)// we are using node.js not browser.
+
+console.log(3
+        +
+        3 )//code readability should be high.
+
+/* console.log("Deep")
+Documentation for JS = tc.es ,  MDN */
+
+//number => 2 to power 53
+//bigint
+// string=>""
+//boolean=> True/false
+//null=> standalone value
+//undefined=> value not assigne
+//symbol=> unique
+//object
+
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
